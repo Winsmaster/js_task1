805eecf21eda47810683aacae37e36d548710166
Winsmaster <amelmurm@yandex.ru>
2022-06-15 19:46:04 +0300

    Задача1

---
+++ task.js
@@ -0,0 +1,22 @@
+"use strict";
+function solveEquation(a, b, c) {
+  let arr = [];  // код для задачи №1 писать здесь
+  let D = b**2 - 4*a*c;
+  if (D < 0) {
+    return arr;
+  } else if (D == 0) {
+    arr = [-b/(2*a)];
+    return arr;
+  } else {
+    arr = [(-b + Math.sqrt(D) )/(2*a), (-b - Math.sqrt(D) )/(2*a)]
+  }
+  return arr; // array
+}
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;  // код для задачи №2 писать здесь
+
+
+
+  return totalAmount;
+}
